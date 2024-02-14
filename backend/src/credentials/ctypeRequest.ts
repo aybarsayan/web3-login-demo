26ccc0eb04a375b3c2d1ee3ba95308a9ab7e25de
Aybars Göktuğ Ayan <37300874+aybarsayan@users.noreply.github.com>
2024-02-14 16:36:46 +0300

    feat: map fix

--- backend/src/credentials/ctypeRequest.ts
+++ backend/src/credentials/ctypeRequest.ts
@@ -6,8 +6,13 @@ import { CTYPE_HASH, REQUIRED_PROPERTIES, TRUSTED_ATTESTERS } from '../config'
 // You can change it by importing a different one.
 // Establish which cTypes our dApp accepts and which attesters we trust:
 
-const trustedAttesters = TRUSTED_ATTESTERS.split(',') as Kilt.DidUri[]
-const requiredProperties = REQUIRED_PROPERTIES.split(',')
+const trustedAttesters = TRUSTED_ATTESTERS.split(',').map((s)=> {
+  const trimmed = s.trim()
+  Kilt.Did.validateUri(trimmed)
+  return trimmed as Kilt.DidUri
+})
+
+const requiredProperties = REQUIRED_PROPERTIES.split(',').map((s)=> s.trim())
 
 /**
  * Credential for users to configure default as SocialKYC Email Credential
